c5d8bd1f047c4a415a21b5308891d97b2c9e7f28
mar-perezcolman <mar.perezcolman@gmail.com>
2023-07-04 20:43:16 +0100

    correcion del ./

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,6 +1,6 @@
 document.getElementById("header").innerHTML=`<nav class="navbar navbar-expand-sm navbar-light bg-light">
 <div class="container">
-<a class="navbar-brand" href="index.html" style="height:100px"><img src="img/LogoCoDEVC.jpg" alt="logo" style="width:60%"></a>
+<a class="navbar-brand" href="index.html" style="height:100px"><img src="./img/LogoCoDEVC.jpg" alt="logo" style="width:60%"></a>
     <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
